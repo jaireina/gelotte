0bfe25234b617a7016ebf39bc02d53fe6d6fb1fa
Bob Martin <bob.martin@spyderbit.com>
2017-05-17 22:46:27 -0700

    Figured out splitting content

--- wp-content/themes/gelotte/front-page.php
+++ wp-content/themes/gelotte/front-page.php
@@ -31,105 +31,113 @@
 
     <body <?php body_class(); ?>>
 
-        <div id="wrapper">
+            <div id="wrapper">
 
-            <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right">
-                <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false, 'menu_class' => 'side-menu', 'walker' => new BootstrapBasicMyWalkerNavMenu())); ?>
-                <?php dynamic_sidebar('navbar-right'); ?>
+                <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right">
+                    <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false, 'menu_class' => 'side-menu', 'walker' => new BootstrapBasicMyWalkerNavMenu())); ?>
+                    <?php dynamic_sidebar('navbar-right'); ?>
 
-                <div class="sidebar-socials">
-                    <?php get_social_icons(); ?>
-                </div>
-            </nav> <!-- End sidebar right menu -->
-
-            <div class="container">
-                <!-- Begin Navigation Section -->
-                <header role="banner">
-                    <div class="row site-branding">
-                        <div class="row main-navigation">
-                            <div class="col-lg-12 site-menu">
-                                <nav class="navbar navbar-default" role="navigation">
-
-                                    <div class="navbar-header">
-                                        <button type="button" class="navbar-toggle toggle-menu menu-right push-body">
-                                            <span class="icon-bar"></span>
-                                            <span class="icon-bar"></span>
-                                            <span class="icon-bar"></span>
-                                        </button>
-
-                                        <div class="site-logo">
-                                            <?php the_custom_logo(); ?>
-                                        </div>
-
-                                        <div class="site-title-container">
-                                            <h1 class="site-title-heading">
-                                                <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-                                            </h1>
-                                        </div>
-                                    </div>
-                                    <div class="navbar-collapse collapse">
-                                        <ul class="nav navbar-nav navbar-right">
-                                            <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav', 'walker' => new BootstrapBasicMyWalkerNavMenu())); ?>
-                                            <?php dynamic_sidebar('navbar-right'); ?>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-                        </div><!--.main-navigation-->
+                    <div class="sidebar-socials">
+                        <?php get_social_icons(); ?>
                     </div>
-                </header><!-- End header section -->
+                </nav> <!-- End sidebar right menu -->
 
-                <div class="content">
-                <!-- End HEADER TEMPLATE SECTION -->
+                <div class="container">
+                    <!-- Begin Navigation Section -->
+                    <header role="banner">
+                        <div class="row site-branding">
+                            <div class="row main-navigation">
+                                <div class="col-lg-12 site-menu">
+                                    <nav class="navbar navbar-default" role="navigation">
 
-                    <!-- Begin Content -->
+                                        <div class="navbar-header">
+                                            <button type="button" class="navbar-toggle toggle-menu menu-right push-body">
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                            </button>
 
-                    <!-- THIS AREA TO BE USED FOR TESTING PURPOSES -->
+                                            <div class="site-logo">
+                                                <?php the_custom_logo(); ?>
+                                            </div>
 
-                    <div class="crop-wrapper">
-                        <div class="wide-wrapper">
-                            <div class="image-wrapper desktop-image">
-                                    <img src="<?php the_field('desktop_image'); ?>" alt="" />
+                                            <div class="site-title-container">
+                                                <h1 class="site-title-heading">
+                                                    <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+                                                </h1>
+                                            </div>
+                                        </div>
+                                        <div class="navbar-collapse collapse">
+                                            <ul class="nav navbar-nav navbar-right">
+                                                <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav', 'walker' => new BootstrapBasicMyWalkerNavMenu())); ?>
+                                                <?php dynamic_sidebar('navbar-right'); ?>
+                                            </ul>
+                                        </div>
+                                    </nav>
+                                </div>
+                            </div><!--.main-navigation-->
+                        </div>
+                    </header><!-- End header section -->
+
+                    <div class="content">
+                    <!-- End HEADER TEMPLATE SECTION -->
+
+                        <!-- Begin Content -->
+
+                        <!-- THIS AREA TO BE USED FOR TESTING PURPOSES -->
+                        <div id="top-content-wrapper">
+
+                            <div class="crop-wrapper">
+                                <div class="wide-wrapper">
+                                    <div class="image-wrapper desktop-image">
+                                            <img src="<?php the_field('desktop_image'); ?>" alt="" />
+                                    </div>
+                                    <div class="image-wrapper mobile-image">
+                                            <img src="<?php the_field('mobile_image'); ?>" alt="" />
+                                    </div>
+                                </div>
                             </div>
-                            <div class="image-wrapper mobile-image">
-                                    <img src="<?php the_field('mobile_image'); ?>" alt="" />
+
+                        </div><!-- End Top Content Wrapper -->
+
+                        <div id="bottom-content-wrapper">
+
+                        </div><!-- End Bottom Content Wrapper -->
+
+                        <!-- THIS AREA TO BE USED FOR TESTING PURPOSES -->
+
+                    <!-- BEGIN FOOTER TEMPLATE SECTION -->
+                    </div><!-- End content -->
+
+                    <footer class="footer">
+                        <div class="container">
+                            <div id="footer-row" class="row site-footer">
+
+                                <div class="site-description">
+                                    <h1><?php bloginfo('description'); ?></h1>
+                                </div>
+
+                                <div class="socials">
+                                    <?php get_social_icons(); ?>
+                                </div>
+
                             </div>
                         </div>
-                    </div>
+                    </footer>
 
-                    <!-- THIS AREA TO BE USED FOR TESTING PURPOSES -->
+            </div><!-- End Footer Container -->
 
-                <!-- BEGIN FOOTER TEMPLATE SECTION -->
-                </div><!-- End content -->
+                <!-- Wordpress Footer-->
+                <?php wp_footer(); ?>
 
-                <footer class="footer">
-                    <div class="container">
-                        <div id="footer-row" class="row site-footer">
+                <!--call jPushMenu, required-->
+                <script>
+                    jQuery(document).ready(function($) {
+                        $('.toggle-menu').jPushMenu();
+                    });
+                </script>
 
-                            <div class="site-description">
-                                <h1><?php bloginfo('description'); ?></h1>
-                            </div>
+            </div><!-- End id="wrapper" -->
 
-                            <div class="socials">
-                                <?php get_social_icons(); ?>
-                            </div>
-
-                        </div>
-                    </div>
-                </footer>
-
-        </div><!-- End Footer Container -->
-
-            <!-- Wordpress Footer-->
-            <?php wp_footer(); ?>
-
-            <!--call jPushMenu, required-->
-            <script>
-                jQuery(document).ready(function($) {
-                    $('.toggle-menu').jPushMenu();
-                });
-            </script>
-
-        </div>
     </body>
 </html>
