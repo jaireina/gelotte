f7df191dba97f987aed539cdbd5c6818b1e6a0c4
Bob Martin <bob.martin@spyderbit.com>
2017-05-18 19:41:53 -0700

    Gallery image resizing started

--- wp-content/themes/gelotte/js/single-project-setup.js
+++ wp-content/themes/gelotte/js/single-project-setup.js
@@ -1,10 +1,8 @@
 function setProjectTopWrapper() {
     
     var viewport_height = jQuery(window).height();
-    var header_height = jQuery('.main-navigation').height();
-    var footer_height = jQuery('.footer').height();
-    var top_content_wrapper = ( viewport_height - header_height );
+    var header_height = jQuery( '.main-navigation' ).height();
+    var footer_height = jQuery( '.footer' ).height();
 
-    jQuery('#top-content-wrapper').css( 'height' , ( viewport_height - header_height ) );
-    jQuery('#slider').css( 'height' , ( top_content_wrapper - 75 ) );
+    jQuery( '#top-content-wrapper' ).css( 'height' , ( viewport_height - header_height ) );
 }
--- wp-content/themes/gelotte/single-project.php
+++ wp-content/themes/gelotte/single-project.php
@@ -10,47 +10,32 @@ get_header();
 ?>
 <!-- Begin Content -->
 <div id="top-content-wrapper">
-    <div id="slider" class="flexslider">
-        <ul class="slides">
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide01.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide02.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide03.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide04.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide05.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide06.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide07.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide08.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide09.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide10.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide11.jpg" />
-            </li>
-            <li>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slide12.jpg" />
-            </li>
-            <!-- items mirrored twice, total of 12 -->
-        </ul>
-    </div>
+
+    <?php
+
+    $images = get_field('desktop_gallery');
+
+    if( $images ): ?>
+        <div id="slider" class="flexslider">
+            <div class="crop-wrapper">
+                <div class="wide-wrapper">
+                    <ul class="slides">
+
+                        <?php foreach( $images as $image ): ?>
+                            <li>
+
+                                    <div class="image-wrapper desktop-image">
+                                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                    </div>
+
+                            </li>
+                        <?php endforeach; ?>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
 
 </div><!-- End Top Content Wrapper -->
 
