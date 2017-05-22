44d8a78dfd3594ea67f9716bc7cda2754d41c539
Bob Martin <bob.martin@spyderbit.com>
2017-05-22 10:30:30 -0700

    Added files for the rest template

---
+++ wp-content/themes/gelotte/the-rest-page.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Template Name: The Rest Page
+ *
+ * @package gelotte
+ */
+
+get_header();
+
+?>
+
+    <!-- Begin Content -->
+    <div id="top-content-wrapper">
+        <div class="top-content-inner">
+
+            <div class="row nopadding">
+
+                <div class="respheight col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    1
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    2
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="respheight col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    1
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    2
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="respheight col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    1
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    2
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 ">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div><!-- End Top Content Wrapper -->
+
+    <div id="bottom-content-wrapper">
+
+    </div><!-- End Bottom Content Wrapper -->
+
+    <!-- BEGIN FOOTER TEMPLATE SECTION -->
+<?php get_footer(); ?>
