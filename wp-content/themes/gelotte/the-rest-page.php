df8ceb94470029499fb351bf7990162b42e627a4
Bob Martin <bob.martin@spyderbit.com>
2017-05-25 00:12:32 -0700

    Updated The Rest Page

--- wp-content/themes/gelotte/the-rest-page.php
+++ wp-content/themes/gelotte/the-rest-page.php
@@ -9,117 +9,117 @@ get_header();
 
 ?>
 
-            <!-- Begin Content -->
-            <div id="top-content-wrapper">
-                <div class="top-content-inner">
+    <!-- Begin Content -->
+    <div id="top-content-wrapper">
+        <div class="top-content-inner">
 
-                    <div class="row nopadding">
+            <div class="row nopadding">
 
-                        <div class="respheight grid-nine-top col-sm-12">
+                <div class="respheight grid-nine-top col-sm-12">
 
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper left">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-top">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-1-500x335.png"/>
-                                        </div>
-                                    </div>
+                    <div class="col-sm-4 grid-nine-col left">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-1-1" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-1-500x335.png"/>
                                 </div>
                             </div>
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper middle">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-middle">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-2-500x335.png"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper right">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-bottom">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-5-500x335.png"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
+                    </div>
 
-                        <div class="respheight grid-nine-middle col-sm-12">
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper left">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-top">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-6-500x335.png"/>
-                                        </div>
-                                    </div>
+                    <div class="col-sm-4 grid-nine-col middle">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-1-2" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-2-500x335.png"/>
                                 </div>
                             </div>
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper middle">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-middle">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-3-500x335.png"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper right">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-bottom">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-4-500x335.png"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
+                    </div>
 
-                        <div class="respheight grid-nine-bottom col-sm-12">
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper left">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-top">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-7-500x335.png"/>
-                                        </div>
-                                    </div>
+                    <div class="col-sm-4 grid-nine-col right">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-1-3" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-3-500x335.png"/>
                                 </div>
                             </div>
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper middle">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-middle">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-8-500x335.png"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-4 grid-nine-col">
-                                <div class="crop-wrapper right">
-                                    <div class="wide-wrapper">
-                                        <div class="grid-nine-image-wrapper-bottom">
-                                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-9-500x335.png"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
-
                     </div>
 
                 </div>
+
+                <div class="respheight grid-nine-middle col-sm-12">
+
+                    <div class="col-sm-4 grid-nine-col left">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-2-1" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-4-500x335.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 grid-nine-col middle">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-2-2" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-5-500x335.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 grid-nine-col right">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-2-3" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-6-500x335.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="respheight grid-nine-bottom col-sm-12">
+
+                    <div class="col-sm-4 grid-nine-col left">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-3-1" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-7-500x335.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 grid-nine-col middle">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-3-2" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-8-500x335.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 grid-nine-col right">
+                        <div class="crop-wrapper">
+                            <div class="wide-wrapper">
+                                <div class="grid-nine-image-wrapper">
+                                    <img class="image-3-3" src="<?php echo get_stylesheet_directory_uri() ?>/images/rest-image-placeholder-9-500x335.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
 
     </div><!-- End Top Content Wrapper -->
 
