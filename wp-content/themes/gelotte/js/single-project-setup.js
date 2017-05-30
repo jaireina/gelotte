e47753b43683f24db06b0c052c13e9d4ce6ace91
Bob Martin <bob.martin@spyderbit.com>
2017-05-30 15:27:55 -0700

    Code cleanup

--- wp-content/themes/gelotte/js/single-project-setup.js
+++ wp-content/themes/gelotte/js/single-project-setup.js
@@ -2,7 +2,6 @@ function setProjectTopWrapper() {
     
     var viewport_height = jQuery(window).height();
     var header_height = jQuery( '.main-navigation' ).height();
-    var footer_height = jQuery( '.footer' ).height();
 
     jQuery( '#top-content-wrapper' ).css( 'height' , ( viewport_height - header_height ) );
 }
